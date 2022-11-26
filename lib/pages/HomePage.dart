74de240c1e08ce9bcae0b63b5e535b9003822864
ManzzEDTZ <manjuraki110@gmail.com>
2022-11-26 20:06:45 +0700

    Maaf ya repositorynya habis ke hapus

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,130 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 7, 50, 99),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(right: 20, left: 15, top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.menu,
+                      color: Color.fromARGB(0, 255, 255, 255),
+                      size: 30,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 7, 50, 99),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.2),
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Badge(
+                        badgeColor: Colors.orange,
+                        padding: EdgeInsets.all(6),
+                        badgeContent: Text(
+                          "2",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            CupertinoIcons.cart,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //Wlecome Text
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome",
+                      style: TextStyle(
+                        fontSize: 35,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "What do You Want To Buy?",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+
+              //Search Bar
+              Container(
+                margin: EdgeInsets.all(15),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search),
+                    Container(
+                      margin: EdgeInsets.only(left: 10),
+                      width: 250,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Search Here...",
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Icon(Icons.filter_list),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                  ),
+                // child: Column(
+                //   crossAxisAlignment: CrossAxisAlignment.start,
+                //   children: [
+                //     Category
+                //   ],
+                // ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/SplashScreen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'HomePage.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        (() => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomePage()))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Image(
+            image: AssetImage("assets/images/BroFood_Logo.png"),
+          ),
+          SpinKitWave(
+            color: Color.fromARGB(255, 7, 50, 99),
+            size: 20.0,
+          ),
+        ],
+      )),
+    );
+  }
+}
